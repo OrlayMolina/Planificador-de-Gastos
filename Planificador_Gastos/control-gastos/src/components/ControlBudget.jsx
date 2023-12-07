57553b090cd773e1bb993d668574272b9478f8f8
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-07 15:45:37 -0500

    Se agrega una grafica de porcentage gastado del presupuesto que estará activa por medio de un useState.

--- Planificador_Gastos/control-gastos/src/components/ControlBudget.jsx
+++ Planificador_Gastos/control-gastos/src/components/ControlBudget.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
 import PropTypes from 'prop-types';
 
 const ControlBudget = ({ expenses, budget }) => {
 
+    const [percentage , setPercentage] = useState(0);
     const [available , setAvailable] = useState(0);
     const [spent , setSpent] = useState(0);
 
     useEffect(() => {
         const totalSpent = expenses.reduce((total, expense) => expense.amount + total, 0);
         const totalAvailable = budget - totalSpent;
+
+        // Spent Percentage
+        const newPercentage = (( (budget - totalAvailable) / budget ) * 100).toFixed(2);
     
         setAvailable(totalAvailable);
         setSpent(totalSpent);
+        setTimeout(() => {
+            setPercentage(newPercentage);
+        }, 1000);
+
     }, [expenses, budget]);
 
     const formatQuantity = quantity => {
@@ -24,7 +34,15 @@ const ControlBudget = ({ expenses, budget }) => {
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-      <p>Gráfica Aquí</p>
+      <CircularProgressbar 
+        styles={buildStyles({
+          pathColor: '#3B82F6',
+          trailColor: '#F5F5F5',
+          textColor: '#3B82F6'
+        })}
+        value={percentage}
+        text={`${percentage}% Gastado`}
+      />
 
       <div className="contenido-presupuesto">
         <p>
